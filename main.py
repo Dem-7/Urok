e5d8ff3d927acfe5c0d475258b73f07b030d6ec9
Dem-7 <dem67713@gmail.com>
2024-12-18 21:56:44 +0300

    j

--- main.py
+++ main.py
@@ -7,23 +7,29 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT ))
 pygame.display.set_caption("Игра тир")
 icon = pygame.image.load("img/иконка3.png")
 pygame.display.set_icon(icon)
-target_image = pygame.image.load("img/яблоко.jpg")
-target_width = 80
-target_height = 80
-target_x = random.randint(0, SCREEN_HEIGHT-target_width)
+target_image = pygame.image.load("img/result_тт.jpg")
+target_width = 40
+target_height = 40
+target_x = random.randint(0, SCREEN_WIDTH-target_width)
 target_y = random.randint(0, SCREEN_HEIGHT-target_height)
+
+
+
 color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-
-
-
-
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x , mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x< target_x +target_width and target_y <mouse_y <target_y +target_height:
+                target_x = random.randint(0, SCREEN_HEIGHT - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-
-
-
+    screen.blit(target_image,(target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
