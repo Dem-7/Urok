d776deff1f2e73878b81b61a2e284be45b77008a
Dem-7 <dem67713@gmail.com>
2024-12-22 20:16:18 +0300

    nn

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import pygame
 import random
 pygame.init()
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT ))
+r= 900
+l = 700
+screen = pygame.display.set_mode((r,l ))
 pygame.display.set_caption("Игра тир")
 icon = pygame.image.load("img/иконка3.png")
 pygame.display.set_icon(icon)
 target_image = pygame.image.load("img/result_тт.jpg")
-target_width = 40
-target_height = 40
-target_x = random.randint(0, SCREEN_WIDTH-target_width)
-target_y = random.randint(0, SCREEN_HEIGHT-target_height)
+nn = 40
+tt = 40
+target_x = random.randint(0, r-nn)
+target_y = random.randint(0, l-tt)
 
 
 
@@ -25,9 +25,9 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x , mouse_y = pygame.mouse.get_pos()
-            if target_x < mouse_x< target_x +target_width and target_y <mouse_y <target_y +target_height:
-                target_x = random.randint(0, SCREEN_HEIGHT - target_width)
-                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+            if target_x < mouse_x< target_x +nn and target_y <mouse_y <target_y + tt:
+                target_x = random.randint(0, l - nn)
+                target_y = random.randint(0, l - tt)
 
     screen.blit(target_image,(target_x, target_y))
     pygame.display.update()
